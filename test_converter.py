d702b3bff33068187f90ab867cf4214f2e6ce1ff
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-04-24 11:34:55 +0200

    Add test if converter converts basic roman numerals to int

--- test_converter.py
+++ test_converter.py
@@ -13,3 +13,7 @@ def test_if_converter_accepts_one_argument():
         converter(argument)
     except TypeError:
         assert False, "Converter accepts one argument"
+
+def test_if_converter_converts_basic_roman_numerals_to_int():
+    numerals = converter("MDCX")
+    assert type(numerals) == int 
