003bc3298fa1d37574626cac277b0fb1725a2e68
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-04-24 10:49:41 +0200

    Add test if converter accepts one argument

--- test_converter.py
+++ test_converter.py
@@ -7,3 +7,9 @@ def test_import_converter():
         assert False, error
 
 
+def test_if_converter_accepts_one_argument():
+    argument = 1
+    try:
+        converter(argument)
+    except TypeError:
+        assert False, "Converter accepts one argument"
