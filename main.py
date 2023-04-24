1983c9e3486c6b4f138a840e98bf94d85560c466
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-04-24 11:36:05 +0200

    Add conversion of basic roman numerals to int

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 
 def converter(number):
-    pass
+    if type(number)==str:
+        number = set(number)
+        symbols = {
+            "M": 1000,
+            "D": 500,
+            "C": 100,
+            "L": 50,
+            "X": 10,
+            "V": 5,
+            "I": 1
+
+        }
+        result = 0
+        for k, v in symbols.items():
+            if k in number:
+                result += v
+        return int(result)
+    
