70860b6f3b01be6c71cdc4d10f799278607b5a69
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-04-24 14:05:42 +0200

    Add conversion of complex int into rn

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 
 def converter(number):
-    symbols = {
-            "M": 1000,
-            "D": 500,
-            "C": 100,
-            "L": 50,
-            "X": 10,
-            "V": 5,
-            "I": 1
-
-        }
+    symbols = {'M': 1000,
+            'CM': 900,
+            'D': 500,
+            'CD': 400,
+            'C': 100,
+            'XC': 90,
+            'L': 50,
+            'XL': 40,
+            'X': 10,
+            'IX': 9,
+            'V': 5,
+            'IV': 4,
+            'I': 1
+            }
     
     result = 0
     for k, v in symbols.items():
@@ -27,9 +31,14 @@ def converter(number):
 
 
         elif type(number)==int:
-            if v == number:
-                result = k
-                return result
-            
+            result = ''
+            for k, v in symbols.items():
+                while number >= v:
+                    result += k
+                    number -= v
+                if number == 0:
+                    break
+            return result
+                    
 
 
