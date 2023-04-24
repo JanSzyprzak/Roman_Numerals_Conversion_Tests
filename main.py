651c8f3f37698d109009ea183be23846ce29fb71
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-04-24 13:34:39 +0200

    Add conversion of complex rn

--- main.py
+++ main.py
@@ -14,11 +14,18 @@ def converter(number):
     result = 0
     for k, v in symbols.items():
         
-        if type(number)==str:
-            if k == number:
-                result += v
-                return int(result)
-        
+        if type(number)==str:     
+            previous_value = 0 
+            for l in number:
+                value = symbols[l]
+                if value > previous_value:
+                    result += value - 2 * previous_value
+                else:
+                    result += value
+                previous_value = value
+            return result
+
+
         elif type(number)==int:
             if v == number:
                 result = k
